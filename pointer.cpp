00988bbdab05aaad438e1d299a2e2a4ebb9605dd
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-05-05 15:19:27 +0700

    menambahkan fungsi utama

--- pointer.cpp
+++ pointer.cpp
@@ -12,3 +12,32 @@ class staf{
     public:
     int nidn;
 };
+int main(){
+    Dosen ds;
+    ds.nama = "Giga";
+    ds.tampilNama();
+
+    Dosen &dsref = ds;
+    dsref.nama = "Joko";
+    cout << "Alamat memori = " << &dsref << endl;
+    dsref.tampilNama();
+
+    Dosen *pds = &ds;
+    pds->nama = "Reza";
+    cout << "Alamat memori = " << pds << endl;
+    pds ->tampilNama();
+
+    int a = 5;
+    int b = 3;
+    int *c = &a;
+    *c = 9;
+    cout << endl;
+    cout << a << endl;
+
+    cout << "Alamat memori a = " << &a << endl;
+    cout << "Alamat memori c = " << c << endl;
+    cout << "Cetak c = " << *c << endl;
+    c = &b;
+    cout << "Alamat memori c = " << c << endl;
+    cout << "Cetak c = " << *c << endl;
+}
