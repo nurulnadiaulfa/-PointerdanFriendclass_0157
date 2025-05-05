a7150828b20919395237efc7bf3bc6a8d2d9ab1c
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-05-05 15:30:24 +0700

    menambahkan object dan pointer reference

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -8,3 +8,14 @@ class mahasiswa{
         cout <<"No Induk = "<<nim<<endl;
     }
 };
+
+int main (){
+
+    mahasiswa mhs; //object mhs
+    mhs.nim = 5;
+    mhs.showNim(); //member access operator
+
+    mahasiswa &refMhs = mhs; //pointer reference refMhs
+    refMhs.nim = 2; //member access operator
+    mhs.showNim();
+}
